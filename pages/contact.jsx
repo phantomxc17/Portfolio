6d0fa362fe417fd6767114cc5a532f18d5fe8227
phantomxc17 <jonathan.sanusi68@gmail.com>
2024-10-03 19:16:46 +0700

    Web

--- pages/contact.jsx
+++ pages/contact.jsx
@@ -32,58 +32,6 @@ const ContactPage = () => {
         <h3 className={styles.heading}>Reach Out Via Socials</h3>
         <ContactCode />
       </div>
-      <div>
-        <h3 className={styles.heading}>Or Fill Out This Form</h3>
-        <form className={styles.form} onSubmit={submitForm}>
-          <div className={styles.flex}>
-            <div>
-              <label htmlFor="name">Name</label>
-              <input
-                type="text"
-                name="name"
-                id="name"
-                value={name}
-                onChange={(e) => setName(e.target.value)}
-                required
-              />
-            </div>
-            <div>
-              <label htmlFor="email">Email</label>
-              <input
-                type="email"
-                name="email"
-                id="email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                required
-              />
-            </div>
-          </div>
-          <div>
-            <label htmlFor="name">Subject</label>
-            <input
-              type="text"
-              name="subject"
-              id="subject"
-              value={subject}
-              onChange={(e) => setSubject(e.target.value)}
-              required
-            />
-          </div>
-          <div>
-            <label htmlFor="message">Message</label>
-            <textarea
-              name="message"
-              id="message"
-              rows="5"
-              value={message}
-              onChange={(e) => setMessage(e.target.value)}
-              required
-            ></textarea>
-          </div>
-          <button type="submit">Submit</button>
-        </form>
-      </div>
     </div>
   );
 };
